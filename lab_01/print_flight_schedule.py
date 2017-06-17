ec3a233269afad764d24fd9023036b7c927693c1
Xiaoyi Lin <xxl163230@utdallas.edu>
2017-06-16 20:12:09 -0500

    the new one

--- lab_01/print_flight_schedule.py
+++ lab_01/print_flight_schedule.py
@@ -1,17 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
-flight_schedule = [['T1','AUS','DAL','0600','0650'],
- ['T2','DAL','HOU','0600','0705'],
-['T3','DAL','HOU','0600','0705']]
-addition = [['T4','HOU','AUS','0600','0650'],['T5','HOU','DAL','0600','0645'],['T6','HOU','DAL','0600','0705']]
-flight_schedule.extend(addition)
-print(flight_schedule)
-
 csv_header = 'tail_number,origin,destination,departure_time,arrival_time'
 file_name = 'flight_schedule.csv'
 
@@ -21,6 +7,11 @@ def print_flight_schedule(fn, csv_hdr, flt_sched):
         for s in flt_sched:
             print(','.join(s), file=f)
 
+flight_schedule = [['T1','AUS','DAL','0600','0650'],
+ ['T2','DAL','HOU','0600','0705'],
+['T3','DAL','HOU','0600','0705']]
+addition = [['T4','HOU','AUS','0600','0645'],['T5','HOU','DAL','0600','0705'],['T6','HOU','DAL','0600','0705']]
+flight_schedule.extend(addition)
+print(flight_schedule)
+
 print_flight_schedule(file_name, csv_header, flight_schedule)
-
-
