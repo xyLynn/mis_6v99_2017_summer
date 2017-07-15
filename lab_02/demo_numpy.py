942ec11596d172a14fbbad69093edf296984419a
Xiaoyi Lin <xxl163230@utdallas.edu>
2017-07-14 19:40:59 -0500

    new edition

--- lab_02/demo_numpy.py
+++ lab_02/demo_numpy.py
@@ -5,6 +5,15 @@ Spyder Editor
 This is a temporary script file.
 """
 
+import sys
 import numpy as np
 a = np.arange(15).reshape(3,5)
+
+sys.stdout = open('demo_numpy0.txt', 'w')
+print(a)
+print(a.shape)
+print(a.size)
+print(a.itemsize)
+print(a.ndim)
+print(a.dtype)
 
